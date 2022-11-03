16a428fe004270b62725079be254f25d93bc3d00
hpweisberg <115960641+hpweisberg@users.noreply.github.com>
2022-11-02 18:51:05 -0700

    Time: 163 ms (38.79%), Space: 42.4 MB (72.78%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+
+    
+    input: array of nums, target (int)
+    output: indices of 2 values from array that = target (int)
+    constraints: at least 2 nums in the array
+                 can't repeate nums
+    edgecase: if 2 nums, then assume they = target
+
+*/
+
+
+var twoSum = function(nums, target) { // declaire varable "twoSum"
+  
+    for (var i = 0; i < nums.length; i++){ // counting the number of intergers in the array
+    
+        for (var j = i + 1; j < nums.length; j++){ // next interger in array counting up
+            var result = target === nums[i] + nums[j] // does the target number equal i & j
+            
+            if (result){
+                return([i,j])
+            }
+        
+        }
+    
+    }
+    
+    
+    
+};
+
+
+// console.log(twoSum(2, 7, 11, 15))
